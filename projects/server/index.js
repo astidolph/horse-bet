9f9b11255cd1d77a3faa7b2c99d4fd30f7ddd160
Adam Stidolph <a.stidolph@gmail.com>
2024-06-02 18:07:25 +0100

    Add sqlite for data storage successfully adding user to db

--- projects/server/index.js
+++ projects/server/index.js
@@ -3,25 +3,54 @@ const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: { origin: "*" },
 });
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
 
 const port = 3000;
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-
-  socket.on("newUser", (newUser) => {
-    console.log(newUser);
-    io.emit("newUser", `${socket.id.substr(0, 2)}: ${newUser}`);
+async function main() {
+  // open the database file
+  const db = await open({
+    filename: "horseBet.db",
+    driver: sqlite3.Database,
   });
 
-  socket.on("startGame", () => {
-    console.log("game started");
-    io.emit("gameStarted");
+  await db.exec("DROP TABLE user");
+
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS user (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE
+    );
+  `);
+
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+
+    socket.on("newUser", async (newUser) => {
+      console.log(newUser);
+      let result;
+      try {
+        // store the message in the database
+        result = await db.run("INSERT INTO user (name) VALUES (?)", newUser);
+      } catch (e) {
+        // TODO handle the failure
+        return;
+      }
+      io.emit("newUser", newUser);
+    });
+
+    socket.on("startGame", () => {
+      console.log("game started");
+      io.emit("gameStarted");
+    });
+
+    socket.on("disconnect", async () => {
+      console.log("a user disconnected!");
+    });
   });
 
-  socket.on("disconnect", () => {
-    console.log("a user disconnected!");
-  });
-});
+  httpServer.listen(port, () => console.log(`listening on port ${port}`));
+}
 
-httpServer.listen(port, () => console.log(`listening on port ${port}`));
+main();
