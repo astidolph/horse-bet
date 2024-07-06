5476d4a2bcbb7ac9fc936f19e8e28b442e27f5b0
Adam Stidolph <a.stidolph@gmail.com>
2024-07-06 14:01:44 +0100

    Remove local storage clear when loading will need to add a new column to gameState for when to clear cache

--- projects/simulation-app/src/app/services/user-service.ts
+++ projects/simulation-app/src/app/services/user-service.ts
@@ -24,7 +24,6 @@ export class UserService {
   }
 
   constructor(private http: HttpClient) {
-    localStorage.clear();
     let users = localStorage.getItem('users');
     if (users) {
       let parsedUsers = JSON.parse(users);
